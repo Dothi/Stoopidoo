f9871b1603f92b269911d803e024965bbb5b67bf
Dothi <jyrihonkakoski@gmail.com>
2016-06-14 12:15:39 +0300

    protoo

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawner : MonoBehaviour {
+
+    int smallBlocks;
+    int mediumBlocks;
+    int largeBlocks;
+
+    public Transform[] prefabs;
+
+    private Transform spawn;
+    private Rect[] rect = { new Rect(50, 310, 100, 50), new Rect(160, 310, 110, 50), new Rect(280, 310, 100, 50) };
+
+    void Start()
+    {
+        smallBlocks = 2;
+        mediumBlocks = 1;
+        largeBlocks = 1;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0) && spawn != null)
+        {
+            var pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            spawn.transform.position = Camera.main.ScreenToWorldPoint(pos);
+
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            spawn = null;
+        }
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+
+        if (e.type == EventType.MouseDown && rect[0].Contains(e.mousePosition) && smallBlocks > 0)
+        {
+            var pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            spawn = Instantiate(prefabs[0], pos, Quaternion.identity) as Transform;
+            smallBlocks--;
+        }
+        if (e.type == EventType.MouseDown && rect[1].Contains(e.mousePosition) && mediumBlocks > 0)
+        {
+            var pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            spawn = Instantiate(prefabs[1], pos, Quaternion.identity) as Transform;
+            mediumBlocks--;
+        }
+        if (e.type == EventType.MouseDown && rect[2].Contains(e.mousePosition) && largeBlocks > 0)
+        {
+            var pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            spawn = Instantiate(prefabs[2], pos, Quaternion.identity) as Transform;
+            largeBlocks--;
+        }
+
+        GUI.Button(rect[0], "Small block x" + smallBlocks);
+        GUI.Button(rect[1], "Medium block x" + mediumBlocks);
+        GUI.Button(rect[2], "Large block x" + largeBlocks);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    Rigidbody2D myRB;
+    public float speed = 1f;
+    public float fan = 1f;
+    float timer;
+    bool moving;
+    bool movingRight;
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>();
+        timer = 0f;
+        moving = false;
+        movingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!moving)
+        {
+            timer += Time.deltaTime;
+        }
+        if(timer >= 5 && !moving)
+        {
+            Debug.Log(timer);
+            moving = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !moving)
+        {
+            Debug.Log("jloj");
+            moving = true;
+        }
+	}
+    void FixedUpdate()
+    {
+        if (moving && movingRight)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);  
+        }
+        else if(moving && !movingRight)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);  
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.tag == "Fan")
+        {
+            //transform.Translate(0, fan * Time.deltaTime, 0);
+            myRB.velocity = new Vector2(0, fan);
+            Debug.Log("puhallin puhuroi");            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            Debug.Log("trappe'd");
+            Debug.Log("gaemover");
+            Destroy(collision.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "Wall" && movingRight)
+        {
+            movingRight = false;
+        }
+        if (other.gameObject.tag == "Wall" && !movingRight)
+        {
+            movingRight = true;
+        }
+    }
+}
