a16555d9a7e029fed55c25cd2588345b7f791683
Dothi <jyrihonkakoski@gmail.com>
2016-06-15 12:20:11 +0300

    Palikoiden rotate toimii

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -7,6 +7,14 @@ public class BlockSpawner : MonoBehaviour {
     int mediumBlocks;
     int largeBlocks;
 
+    public float speed;
+    public float friction;
+    public float lerpSpeed;
+    public float xDeg;
+    public float yDeg;
+    Quaternion fromRotation;
+    Quaternion toRotation;
+
     public bool isRotating = false;
     public bool isDragging = false;
     public static BlockSpawner instance;
@@ -35,21 +43,40 @@ public class BlockSpawner : MonoBehaviour {
 
     void Update()
     {
-
+        
         if (Input.GetMouseButton(0) && spawn != null)
         {
-            
             var pos = Input.mousePosition;
             pos.z = -Camera.main.transform.position.z;
-            if (Input.GetKeyDown(KeyCode.Z))
+            if (Input.GetKey(KeyCode.Z) && spawn != null)
             {
                 isRotating = true;
             }
-            else if(Input.GetKeyUp(KeyCode.Z))
+            else
             {
                 isRotating = false;
             }
-            spawn.transform.position = Camera.main.ScreenToWorldPoint(pos);
+            if (isRotating)
+            {
+                Vector3 newPos = spawn.transform.position;
+                spawn.transform.position = newPos;
+               /* xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+                yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+                fromRotation = spawn.transform.rotation;
+                toRotation = Quaternion.Euler(0, 0, yDeg);
+                spawn.transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);*/
+
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+                Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+                lookPos = lookPos - spawn.transform.position;
+                float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+                spawn.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            }
+            else
+            {
+                spawn.transform.position = Camera.main.ScreenToWorldPoint(pos);
+            }
             isDragging = true;
         }
         if (Input.GetMouseButtonUp(0))
