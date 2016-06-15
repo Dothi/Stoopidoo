c461cc0699f1312b30fef2d1bc56f0406f0a6693
Dothi <jyrihonkakoski@gmail.com>
2016-06-15 15:57:48 +0300

    pelaajan rotatio slopejen kanssa toimii (jotenki)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,39 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
     Rigidbody2D myRB;
     public float speed;
     public float fan = 1f;
     float timer;
     public float idleTimer;
     bool moving;
+    public bool isGrounded;
     public bool movingRight;
     public bool isTouchingWall;
     RaycastHit2D hit;
     SpriteRenderer spriteRend;
     LayerMask layerMask;
 
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         myRB = GetComponent<Rigidbody2D>();
         timer = 0f;
         moving = false;
         movingRight = true;
         isTouchingWall = false;
+        isGrounded = false;
         spriteRend = GetComponent<SpriteRenderer>();
         idleTimer = 0f;
+        layerMask = 1 << LayerMask.NameToLayer("Player");
+        layerMask = ~layerMask;
     }
-	
-	// Update is called once per frame
-	void Update () 
+
+    // Update is called once per frame
+    void Update()
     {
         if (movingRight)
         {
-            
+
             spriteRend.flipX = false;
-            
+
             if (moving && myRB.velocity.x == 0f)
             {
                 idleTimer += 1 * Time.deltaTime;
@@ -46,7 +52,7 @@ public class Movement : MonoBehaviour {
         }
         else
         {
-            
+
             spriteRend.flipX = true;
 
             if (moving && myRB.velocity.x == 0f)
@@ -57,31 +63,56 @@ public class Movement : MonoBehaviour {
                     movingRight = true;
                     idleTimer = 0f;
                 }
-                
+
             }
         }
         if (!moving)
         {
             timer += Time.deltaTime;
         }
-        if(timer >= 5 && !moving)
+        if (timer >= 5 && !moving)
         {
             Debug.Log(timer);
             moving = true;
         }
-        if(Input.GetKeyDown(KeyCode.Mouse0) && !moving)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !moving)
         {
             Debug.Log("jloj");
             moving = true;
-        }  
-	}
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 1.2f, layerMask);
+        
+
+        if (hit && hit.transform.tag == "Ground")
+        {
+            Debug.Log(hit.transform);
+            isGrounded = true;
+        }
+        
+        
+        else
+        {
+            isGrounded = false;
+        }
+        if (isGrounded)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector2.up, hit.normal), 5f * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector2.up, Vector3.zero), 5f * Time.deltaTime);
+        }
+        
+
+        
+    }
     void FixedUpdate()
     {
         if (moving && movingRight)
         {
             myRB.velocity = new Vector2(speed * Time.deltaTime, myRB.velocity.y);
         }
-        else if(moving && !movingRight)
+        else if (moving && !movingRight)
         {
             myRB.velocity = new Vector2(-speed * Time.deltaTime, myRB.velocity.y);
         }
@@ -105,5 +136,5 @@ public class Movement : MonoBehaviour {
             Debug.Log("gaemover");
             Destroy(collision.gameObject);
         }
-    }   
+    }
 }
---
+++ Assets/Scripts/TrampolineScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineScript : MonoBehaviour
+{
+
+    public float jumpForce = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+    }
+}
+
+
