e12fd738243e66c235781e144d780a537bb8f44d
Dothi <jyrihonkakoski@gmail.com>
2016-06-22 11:57:26 +0300

    MapMenu ja mobile touchinput alulle

--- Assets/Scripts/MapMenu.cs
+++ Assets/Scripts/MapMenu.cs
@@ -14,10 +14,28 @@ public class MapMenu : MonoBehaviour
         var levelObjects = GameObject.FindGameObjectsWithTag("Level");
         var levelCount = levelObjects.Length;
 
-        for (int i = 0; i < levelObjects.Length; i++ )
+        System.Array.Reverse(levelObjects);
+
+        for (int i = 0; i < levelObjects.Length; i++)
         {
-            if(!levels.Contains(levelObjects[i]))
-            levels.Add(levelObjects[i]);
+            if (!levels.Contains(levelObjects[i]))
+            {
+                levels.Add(levelObjects[i]);
+            }
         }
+    }
+
+    void Update()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (ray && ray.collider == levels[0].GetComponent<CircleCollider2D>())
+            {
+                Debug.Log("jee");
+                Application.LoadLevel("JonnaTest");
+            }
+        }
+
     }
 }
