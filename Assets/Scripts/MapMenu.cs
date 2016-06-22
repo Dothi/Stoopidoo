1b4cf2bb88748bbf6369cb9e0e7155f09fa66210
Dothi <jyrihonkakoski@gmail.com>
2016-06-22 09:55:18 +0300

    MenuMap alulle

---
+++ Assets/Scripts/MapMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapMenu : MonoBehaviour
+{
+
+    public List<GameObject> levels;
+
+    void Start()
+    {
+        levels = new List<GameObject>();
+
+        var levelObjects = GameObject.FindGameObjectsWithTag("Level");
+        var levelCount = levelObjects.Length;
+
+        for (int i = 0; i < levelObjects.Length; i++ )
+        {
+            if(!levels.Contains(levelObjects[i]))
+            levels.Add(levelObjects[i]);
+        }
+    }
+}
