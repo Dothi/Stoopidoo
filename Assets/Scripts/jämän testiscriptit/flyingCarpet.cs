cca4870056d12cefbedb7ee21370ccdbc8076773
juhamoilanen <juhamoilanen1@kamk.fi>
2016-07-22 12:32:55 +0300

    lentäviin mattoihin pistetty skripti, accept buttoni vaihdettu vihreäksi

--- Assets/Scripts/jämän testiscriptit/flyingCarpet.cs
+++ Assets/Scripts/jämän testiscriptit/flyingCarpet.cs
@@ -3,96 +3,136 @@ using System.Collections;
 
 public class flyingCarpet : MonoBehaviour {
 
-    float rayLength = 5;
+    public float verticalRayLength = 5;
+    public float horizontalRayLength = 5;
     public float speed = 1f;
     public int distance;
     LayerMask layermask;
     LayerMask playermask;
     public Vector3 endpos;
-    public bool up, down, left, right;
+    public Vector3 playerpos;
+    public GameObject Player;
+    public bool up, down, left, right, moving, playerMove, moved;
     int upcount, downcount, leftcount, rightcount = 0;
     public float timer;
 	// Use this for initialization
 	void Start () {
+        Player = GameObject.Find("Player");
         layermask = 1 << LayerMask.NameToLayer("Hurricane");
         playermask = 1 << LayerMask.NameToLayer("Player");
         up = false;
         down = false;
         left = false;
         right = false;
+        playerMove = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        if (!playerMove)
+        {
+            Movement.instance.moving = false;
+            Movement.instance.anim.SetFloat("speed", 0);
+        }
+        //if(moving)
+        //{
+        //    timer += Time.deltaTime;
+        //    if(timer >= 3)
+        //    {
+        //        Movement.instance.moving = true;
+        //    }
+        //}
         RaycastHit2D uphit;
         RaycastHit2D downhit;
         RaycastHit2D lefthit;
         RaycastHit2D righthit;
         RaycastHit2D hit;
-        hit = Physics2D.Raycast(transform.position, Vector2.up, rayLength, playermask);
-        uphit = Physics2D.Raycast(transform.position, Vector2.up, rayLength, layermask);
-        downhit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, layermask);
-        lefthit = Physics2D.Raycast(transform.position, Vector2.left, rayLength, layermask);
-        righthit = Physics2D.Raycast(transform.position, Vector2.right, rayLength, layermask);
-        Debug.DrawRay(transform.position, Vector2.up * rayLength, Color.red);
-        Debug.DrawRay(transform.position, Vector2.down * rayLength, Color.red);
-        Debug.DrawRay(transform.position, Vector2.left * rayLength, Color.red);
-        Debug.DrawRay(transform.position, Vector2.right * rayLength, Color.red);
-        if(hit)
+        hit = Physics2D.Raycast(transform.position, Vector2.up, verticalRayLength, playermask);
+        uphit = Physics2D.Raycast(transform.position, Vector2.up, verticalRayLength, layermask);
+        downhit = Physics2D.Raycast(transform.position, Vector2.down, verticalRayLength, layermask);
+        lefthit = Physics2D.Raycast(transform.position, Vector2.left, horizontalRayLength, layermask);
+        righthit = Physics2D.Raycast(transform.position, Vector2.right, horizontalRayLength, layermask);
+        Debug.DrawRay(transform.position, Vector2.up * verticalRayLength, Color.red);
+        Debug.DrawRay(transform.position, Vector2.down * verticalRayLength, Color.red);
+        Debug.DrawRay(transform.position, Vector2.left * horizontalRayLength, Color.red);
+        Debug.DrawRay(transform.position, Vector2.right * horizontalRayLength, Color.red);
+        if(hit && !moved)
         {
-            Movement.instance.moving = false;
-            Movement.instance.anim.SetFloat("speed", 0);
+            //Movement.instance.moving = false;
+            playerMove = false;
             timer += Time.deltaTime;
-            if(timer >= 3)
+            if(timer >= 3 && !moving)
             {
+                playerMove = true;
                 Movement.instance.moving = true;
             }
         }
         if(uphit)
         {
-            
+            //Movement.instance.moving = false;
+            playerMove = false;
             if (!down)
             {
+               
                 endpos = transform.position;
                 endpos.y += -distance;
+                playerpos = Player.transform.position;
+                playerpos.y += -distance;
                 Debug.Log("Ylhäältä osu");
+                moving = true;
             }
             
             down = true;
         }
         if (downhit)
         {
-            
+            //Movement.instance.moving = false;
+            playerMove = false;
             if (!up)
             {
+                
                 endpos = transform.position;
                 endpos.y += distance;
+                playerpos = Player.transform.position;
+                playerpos.y += distance;
                 Debug.Log("alhaalta osu");
+                moving = true;
             }
             
             up = true;
         }
         if (lefthit)
         {
-            
+            //Movement.instance.moving = false;
+            playerMove = false;
             if (!right)
             {
+                
                 endpos = transform.position;
                 endpos.x += distance;
+                playerpos = Player.transform.position;
+                playerpos.x += distance;
                 Debug.Log("vasemmalta osu");
+                moving = true;
             }
             
             right = true;
         }
         if (righthit)
         {
+            //Movement.instance.moving = false;
+            playerMove = false;
             
-            if(!left)
+            if (!left)
             {
+                
                 endpos = transform.position;
                 endpos.x += -distance;
+                playerpos = Player.transform.position;
+                playerpos.x += -distance;
                 Debug.Log("oikeelta osu");
+                moving = true;
             }
             
             
@@ -105,42 +145,55 @@ public class flyingCarpet : MonoBehaviour {
     }
     void movingUp()
     {
-        
+        Player.transform.position = Vector3.MoveTowards(Player.transform.position, playerpos, speed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, endpos, speed * Time.deltaTime);
         if(transform.position == endpos)
         {
             upcount = 1;
             up = false;
+            moving = false;
+            playerMove = true;
+            Movement.instance.moving = true;
+            moved = true;
         }
     }
     void movingDown()
     {
-        
+        Player.transform.position = Vector3.MoveTowards(Player.transform.position, playerpos, speed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, endpos, speed * Time.deltaTime);
         if(transform.position == endpos)
         {
             downcount = 1;
-            down = false;
+            down = false; moving = false;
+            playerMove = true;
+            Movement.instance.moving = true;
+            moved = true;
         }
     }
     void movingRight()
     {
-        
+        Player.transform.position = Vector3.MoveTowards(Player.transform.position, playerpos, speed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, endpos, speed * Time.deltaTime);
         if (transform.position == endpos)
         {
             rightcount = 1;
-            right = false;
+            right = false; moving = false;
+            playerMove = true;
+            Movement.instance.moving = true;
+            moved = true;
         }
     }
     void movingLeft()
     {
-        
+        Player.transform.position = Vector3.MoveTowards(Player.transform.position, playerpos, speed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, endpos, speed * Time.deltaTime);
         if(transform.position == endpos)
         {
             leftcount = 1;
-            left = false;
+            left = false; moving = false;
+            playerMove = true;
+            Movement.instance.moving = true;
+            moved = true;
         }
     }
 }
