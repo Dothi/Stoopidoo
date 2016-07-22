39220cb5c18ac2af057f5e1b59db1f0338f311b0
juhamoilanen <juhamoilanen1@kamk.fi>
2016-07-22 14:34:57 +0300

    mainmenuu & darude prefab

---
+++ Assets/Scripts/jämän testiscriptit/MainMenuUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour {
+
+    public Image credits, settings;
+    public Button backbutton, closeSettings;
+    //public Button closeSettings;
+    public Toggle music, sound;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Credits()
+    {
+        credits.gameObject.SetActive(true);
+        backbutton.gameObject.SetActive(true);
+    }
+    public void BackButton()
+    {
+        credits.gameObject.SetActive(false);
+        backbutton.gameObject.SetActive(false);
+    }
+    public void Settings()
+    {
+        settings.gameObject.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settings.gameObject.SetActive(false);
+    }
+}
