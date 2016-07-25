33a92b64e486e9f4ffa28f69417254c82718cb00
Dothi <jyrihonkakoski@gmail.com>
2016-07-25 15:05:39 +0300

    buttonboxit ehkä fixed

--- Assets/Scripts/ButtonBoxCollision.cs
+++ Assets/Scripts/ButtonBoxCollision.cs
@@ -9,14 +9,7 @@ public class ButtonBoxCollision : MonoBehaviour
     {
         bb = GameObject.FindGameObjectWithTag("ButtonBoxController").GetComponent<ButtonBox>();
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-        if (collision.gameObject.tag == "ButtonBox")
-        {
-            bb.Rotating = true;
-        }
-    }
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "ButtonBox")
