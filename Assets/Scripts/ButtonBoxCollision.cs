52662de3a1005ba4f7e1107023b9ef50e83b0da2
Dothi <jyrihonkakoski@gmail.com>
2016-07-26 10:33:38 +0300

    Buttonboxit toivottavasti korjattu ja japsi kentis platformien spritet
    lul

--- Assets/Scripts/ButtonBoxCollision.cs
+++ Assets/Scripts/ButtonBoxCollision.cs
@@ -5,16 +5,28 @@ public class ButtonBoxCollision : MonoBehaviour
 {
     ButtonBox bb;
 
+    public bool rotated;
+
     void Start()
     {
         bb = GameObject.FindGameObjectWithTag("ButtonBoxController").GetComponent<ButtonBox>();
+        rotated = false;
     }
     
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "ButtonBox")
+        if (collision.gameObject.tag == "ButtonBox" && !rotated)
         {
             bb.Rotating = true;
+            rotated = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ButtonBox" && rotated)
+        {
+            
+            rotated = false;
         }
     }
 }
