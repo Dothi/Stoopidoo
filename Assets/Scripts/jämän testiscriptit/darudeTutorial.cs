8d128bf7acac188502f711d022a87d735b5b7b82
juhamoilanen <juhamoilanen1@kamk.fi>
2016-08-03 19:37:48 +0300

    asdf

--- Assets/Scripts/jämän testiscriptit/darudeTutorial.cs
+++ Assets/Scripts/jämän testiscriptit/darudeTutorial.cs
@@ -4,19 +4,24 @@ using UnityEngine.UI;
 public class darudeTutorial : MonoBehaviour {
 
     public Image SandStorm;
+    public bool pause;
 	// Use this for initialization
 	void Start () {
-        GameManager.instance.pauseState = true;
+        pause = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+	if(pause)
+        {
+            GameManager.instance.pauseState = true;
+        }
 	}
 
     public void Close()
     {
+        pause = false;
         SandStorm.gameObject.SetActive(false);
         GameManager.instance.pauseState = false;
     }
